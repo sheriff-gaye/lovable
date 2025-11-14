d7ddf0803b1ac2cc0229817486450570771a1982
gayetech <sheriffgaye5@gmail.com>
2025-11-14 12:03:47 +0000

    chapter two completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-export default function Home() {
+const Home = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "from SSG" }));
+
   return (
+  <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<div>Loading...</div>}>
+  <Client/>
+    </Suspense>
+  </HydrationBoundary>);
+};
 
-    <div>
-      hello world
-    </div>
- 
-  );
-}
+export default Home;
