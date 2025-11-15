63db146dc3deb45310cb760567a3fc561f4495c4
gayetech <sheriffgaye5@gmail.com>
2025-11-15 19:02:00 +0000

    commit today

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,10 +1,23 @@
 import { inngest } from "./client";
+import { Agent, agenticOpenai as openai, createAgent } from "@inngest/agent-kit";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
+
+
+
+     const writer = createAgent({
+      name: "writer",
+      system: "You are an expert writer.  You write readable, concise, simple content.",
+      model: openai({ model: "gpt-4o", step }),
+    });
+
+
+
+
     return { message: `Hello ${event.data.email}!` };
   },
 );
