2358b85f0f564bff7fdf97873576a0edd12b154c
gayetech <sheriffgaye5@gmail.com>
2025-11-22 23:45:07 +0000

    script to create the sandbox

--- sandbox-templates/nextjs/compile_page.sh
+++ sandbox-templates/nextjs/compile_page.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This script runs during building the sandbox template
-# and makes sure the Next.js app is (1) running and (2) the `/` page is compiled
 function ping_server() {
 	counter=0
 	response=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:3000")
